ffd6213972fc4b2fee64f4f25d07d665ea8df7e5
Mynor Morales <mynordma@gmail.com>
2024-08-08 22:34:09 -0600

    clase archivo 1

--- GestorCC/src/main/java/backend/Archivo.java
+++ GestorCC/src/main/java/backend/Archivo.java
@@ -1,5 +1,27 @@
 package backend;
 
+/**
+ * Clase que gestiona las instrucciones realizadas por el usuario y las devuelve para
+ * que la clase Gestion se encargue de ellas.
+ * Cuando las instrucciones ingresan por un archivo de texto, esta clase se encarga de
+ * leer el archivo y devolver las instrucciones.
+ * Cuando las instrucciones ingresan por la interfaz gráfica, esta clase se encarga de
+ * simular las instrucciones que vendrian en un archivo y realizar el mismo proceso.
+ */
 public class Archivo {
+    private String pathEntrada;
+    private String contenido;
+    private String[] instrucciones;
 
+    private void separarContenido() {
+        // Se separa el contenido del archivo en instrucciones
+    }
+
+    public String[] instrucciones() {
+        return instrucciones;
+    }
+
+    public void setPathEntrada(String pathEntrada) {
+        this.pathEntrada = pathEntrada;
+    }
 }
